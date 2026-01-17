1274cdc01f6afd96d195af973e3d66320de371a5
studio-edul <easyone.work@gmail.com>
2026-01-17 17:50:11 +0900

    fix/basePath for github pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 // Force restart
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  // GitHub Pages 배포를 위한 설정
+  basePath: isProd ? '/dul-works' : '',
+  assetPrefix: isProd ? '/dul-works' : '',
   images: {
     unoptimized: true,
     remotePatterns: [
